b404917bf4ee01a8e8ea6f586f56742a10911156
MorhafGhziel <GhzielMorhaf@gmail.com>
2024-09-17 16:33:12 +0300

    created the header

---
+++ src/sections/Header.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Button } from "../components/button";
+
+export const Header = () => {
+  const handleScrollToFeatures = () => {
+    const featuresSection = document.getElementById("features-section");
+    if (featuresSection) {
+      featuresSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  return (
+    <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
+      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
+      <div className="container">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto relative">
+          <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div>
+          <div onClick={handleScrollToTop} className="cursor-pointer">
+            <div className="border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/15"></div>
+          </div>
+          <div className="hidden md:block">
+            <nav className="flex gap-8 text-white/70 text-sm">
+              <a
+                href="#features"
+                className="hover:text-white transition"
+                onClick={handleScrollToFeatures}
+              >
+                Features
+              </a>
+              <a href="#" className="hover:text-white transition">
+                Developers
+              </a>
+              <a href="#" className="hover:text-white transition">
+                Pricing
+              </a>
+              <a href="#" className="hover:text-white transition">
+                Changelog
+              </a>
+            </nav>
+          </div>
+          <div className="flex gap-4 items-center">
+            <Button>Join waitlist</Button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
